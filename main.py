c1891d34b6d799464cf1a2c6df10e2215e4babc1
plachert <pmlachert@gmail.com>
2023-09-22 18:25:20 +0200

    Add endpoints

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from model.model import __version__ as model_version
+from model.model import dummy_predict
 
 
 app = FastAPI()
@@ -8,5 +10,15 @@ class InputData(BaseModel):
     text: str
     
 class Prediction(BaseModel):
-    text: str
+    dummy_prediction: str
+    
+@app.get('/')
+def read_root():
+    return {f"Model version: {model_version}"}
+
+@app.post('/predict', response_model=Prediction)
+def predict(input_data: InputData):
+    prediction = dummy_predict(input_data.text)
+    return {"dummy_prediction": prediction}
+    
 
--- model/model.py
+++ model/model.py
@@ -1,3 +1,5 @@
 
+__version__ = "0.1.0"
+
 def dummy_predict(text):
     return "Dummy output"
