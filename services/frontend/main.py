41609d70bfbcdb195e489f3d733e6fe31a4edd74
plachert <pmlachert@gmail.com>
2023-09-28 23:43:03 +0200

    Add annotation

--- services/frontend/main.py
+++ services/frontend/main.py
@@ -1,17 +1,17 @@
 import streamlit as st
 from annotated_text import annotated_text
+import requests
+from text_utils import annotate_text
+import json
 
-annotated_text(
-    "This ",
-    ("is", "verb"),
-    " some ",
-    ("annotated", "adj"),
-    ("text", "noun"),
-    " for those of ",
-    ("you", "pronoun"),
-    " who ",
-    ("like", "verb"),
-    " this sort of ",
-    ("thing", "noun"),
-    "."
-)
+
+def call_api(text):
+    input_data = {"text": text}
+    response = requests.post(url="http://127.0.0.1:8000/predict", data=json.dumps(input_data))
+    annotated_text = annotate_text(text, response.json()["named_entities"])
+    return annotated_text
+
+
+text = "Warsaw is the capitol of Poland."
+
+annotated_text(call_api(text))
--- services/frontend/text_utils.py
+++ services/frontend/text_utils.py
@@ -1,6 +1,18 @@
 
+
 def annotate_text(text, named_entities):
     annotated_text = []
+    end = 0
+    for named_entity in named_entities:
+        start = named_entity["start"]
+        if text[end:start]:
+            annotated_text.append(text[end:start])
+        end = named_entity["end"]
+        entity = named_entity["entity"]
+        annotated_text.append((text[start:end], entity))
+    return annotated_text
+
+        
     # "This ",
     # ("is", "verb"),
     # " some ",
